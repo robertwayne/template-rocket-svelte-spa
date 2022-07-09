aaeaddae8aba2970b938474b6185cb1a6b29d935
Rob Wagner <rob@sombia.com>
2022-07-09 11:54:23 -0400

    Navbar styling; make the 'logo' a link

--- client/src/components/Nav.svelte
+++ client/src/components/Nav.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <nav class="flex flex-row justify-between font-bold">
-    <h1>Template App</h1>
-    <ul class="flex flex-row">
-        <li class="px-2">
+    <h1 class="text-3xl"><Link to="/">Template App</Link></h1>
+    <ul class="flex flex-row text-xl">
+        <li class="px-2 hover:text-blue-500 hover:duration-300">
             <Link to="/">Home</Link>
         </li>
-        <li class="px-2">
+        <li class="px-2 hover:text-blue-500 hover:duration-300">
             <Link to="/about">About</Link>
         </li>
     </ul>
