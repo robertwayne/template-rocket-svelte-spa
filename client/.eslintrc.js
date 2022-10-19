84ef5b0e4aa94f2d050cf58334e7a252a4336b6b
Rob Wagner <rob@sombia.com>
2022-10-19 04:51:53 -0400

    Update config files

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -7,6 +7,31 @@ module.exports = {
     plugins: ["svelte3", "@typescript-eslint"],
     overrides: [
         {
+            files: ["*.ts"],
+            extends: [
+                "eslint:recommended",
+                "plugin:@typescript-eslint/recommended",
+            ],
+            rules: {
+                "@typescript-eslint/array-type": [
+                    "error",
+                    {
+                        default: "generic",
+                    },
+                ],
+                "no-undef": "off",
+                "no-unused-vars": "off",
+                "@typescript-eslint/no-unused-vars": [
+                    "error",
+                    {
+                        argsIgnorePattern: "^_",
+                        varsIgnorePattern: "^_",
+                    },
+                ],
+                "@typescript-eslint/no-empty-function": "warn",
+            },
+        },
+        {
             files: ["*.svelte"],
             processor: "svelte3/svelte3",
             extends: [
@@ -21,27 +46,12 @@ module.exports = {
                     },
                 ],
                 "no-undef": "off",
-            },
-        },
-        {
-            files: ["*.ts"],
-            extends: [
-                "eslint:recommended",
-                "plugin:@typescript-eslint/recommended",
-            ],
-            rules: {
-                "@typescript-eslint/array-type": [
-                    "error",
-                    {
-                        default: "generic",
-                    },
-                ],
-                "no-undef": "off",
+                "no-unused-vars": "off",
             },
         },
     ],
     rules: {},
     settings: {
-        "svelte3/typescript": () => require("typescript"),
+        "svelte3/typescript": require("typescript"),
     },
 }
